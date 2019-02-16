aacf045b68b73a01bf4a5e91b1fe30defeb12ac1
Nick Niehoff <gitlab@nickniehoff.net>
2019-02-16 13:09:41 -0700

    Getting further

--- model.py
+++ model.py
@@ -6,6 +6,9 @@ class Model:
     latest_juju_version = "2.5.1"
 
     def __init__(self, modelinfo, controller):
+        self.notes = []
+        self.applications = []
+        self.machines = []
         self.name = modelinfo["name"]
         self.type = modelinfo["type"]
         self.controller = controller
@@ -13,11 +16,25 @@ class Model:
         self.cloud = modelinfo["cloud"]
         self.version = modelinfo["version"]
         self.modelstatus = modelinfo["model-status"]["current"]
+        # TODO Convert to datetime
         self.since = modelinfo["model-status"]["since"]
-        if 'meter-status' in modelinfo:
+        if "meter-status" in modelinfo:
             self.meterstatus = modelinfo["meter-status"]["color"]
             self.message = modelinfo["meter-status"]["message"]
         else:
             self.meterstatus = ""
             self.message = ""
         self.sla = modelinfo["sla"]
+
+    def add_application(self, application):
+        self.applications.append(application)
+
+    def add_machine(self, machine):
+        self.machines.append(machine)
+
+    def get_machine(self, machinename):
+        for machine in self.machines:
+            if machine.name == machinename:
+                return machine
+        else:
+            return None
